d681fc6a4882a170e2d2cdfdf121f12e4e472cd2
dion-zebua <zebuadbless@gmail.com>
2024-09-21 17:18:52 +0700

    malas

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,18 +1,18 @@
 @if ($errors->any())
-    <div class="animate-alert bg-white pt-2 pr-2 shadow-2xl z-[99] border-l-8 border-l-blue-700 fixed bottom-5 right-5"
+    <div class="animate-alert max-w-[min(80%,300px)] w-auto bg-gray-100 shadow-2xl z-[99] border-l-8 border-l-blue-700 fixed bottom-5 right-5"
         id="alert">
-        <div class="relative">
-            <button id="closeAlert" class="absolute right-0 top-0">
+        <div class="relative flex justify-between flex-row-reverse items-start p-4 pr-2">
+            <button id="closeAlert" class="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class=" size-5 stroke-blue-700">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
             </button>
-            <div class="text-[13px] p-4 pr-6">
+            <div class="text-[13px] pr-6">
                 <ul>
                     @foreach ($errors->all() as $item)
-                        <li class="{{ $loop->count == 1 ? '' : 'list-decimal' }} ml-2">{{ $item }}</li>
+                        <li class="{{ $loop->count == 1 ? '' : 'list-disc' }} ml-2">{{ $item }}</li>
                     @endforeach
                 </ul>
             </div>
