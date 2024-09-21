ee22904b9a968b9599b38d653e91e412bdf989b2
dion-zebua <zebuadbless@gmail.com>
2024-09-21 17:35:24 +0700

    malas

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,10 +1,12 @@
 @if ($errors->any())
-    <div class="animate-alert max-w-[min(80%,300px)] w-auto bg-gray-100 shadow-2xl z-[99] border-l-8 border-l-blue-700 fixed bottom-5 right-5"
+    <div class="animate-alert max-w-[min(80%,300px)] w-auto bg-gray-100 shadow-2xl z-[99] border-l-[10px] border-l-blue-700 fixed bottom-5 right-5"
         id="alert">
+        <x-animate-expand />
+
         <div class="relative flex justify-between flex-row-reverse items-start p-4 pr-2">
             <button id="closeAlert" class="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class=" size-5 stroke-blue-700">
+                    stroke="currentColor" class=" size-5 stroke-blue-500">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
@@ -17,6 +19,7 @@
                 </ul>
             </div>
         </div>
+
     </div>
     <script>
         const alert = document.querySelector('#alert')
@@ -28,7 +31,7 @@
             })
             setInterval(() => {
                 alert.classList.add('!hidden')
-            }, 5900);
+            }, 6000);
         }
     </script>
 @endif
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
   theme: {
     extend: {
       animation: {
-        alert: 'alert 6s ease-in-out',
+        alert: 'alert 6.1s ease-in-out',
+        expand: 'expand 6.1s ease-in-out',
       },
       keyframes: {
         alert: {
@@ -18,7 +19,13 @@ export default {
           '10%': { transform: 'translateY(0px)' },
           '90%': { transform: 'translateY(0px)' },
           '100%': { transform: 'translateY(50px)' },
-        }
+        },
+        expand: {
+          '0%': { width: '100%' },
+          '5%': { width: '100%' },
+          '90%': { width: '0%' },
+          '100%': { width: '0%' },
+        },
       },
       fontFamily: {
         poppins: '"Poppins", sans-serif',
