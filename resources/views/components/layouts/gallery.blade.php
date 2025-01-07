49de9a8fdfa43fc870c60f8131ebfd2c6de59890
dion-zebua <zebuadbless@gmail.com>
2025-01-07 22:01:19 +0700

    malas

---
+++ resources/views/components/layouts/gallery.blade.php
@@ -0,0 +1,18 @@
+<div class="__container">
+    <div class="text-center inner-container">
+        <h2 class="line-h2">Galeri Travel</h2>
+        <p class="desc">
+            Galeri ini menampilkan berbagai unit, foto, dan momen berharga
+            yang merefleksikan perjalanan kami.
+        </p>
+    </div>
+    <div
+        class="grid grid-cols-12 sm:gap-x-6 gap-y-8 [&amp;>*]:overflow-hidden [&amp;>img]:cursor-pointer [&amp;>img]:w-full [&amp;>img]:object-cover [&amp;>img]:rounded-xl [&amp;>img]:shadow-lg [&amp;>img]:h-72 [&amp;>img]:transition-all [&amp;>img]:duration-700 [&amp;>img]:brightness-50 hover:[&amp;>img]:brightness-90 [&amp;>img]:col-span-full">
+        <img width="100" height="100" src="./src/img/galeri/hiace.jpg" class="sm:!col-span-8" alt="galeri 1">
+        <img width="100" height="100" src="./src/img/galeri/surabaya.jpg" class="sm:!col-span-4" alt="galeri 2">
+        <img width="100" height="100" src="./src/img/galeri/bali.jpg" class="sm:!col-span-4" alt="galeri 3">
+        <img width="100" height="100" src="./src/img/galeri/avanza.jpg" class="sm:!col-span-8" alt="galeri 4">
+        <img width="100" height="100" src="./src/img/galeri/grandmax.jpg" class="sm:!col-span-6" alt="galeri 5">
+        <img width="100" height="100" src="./src/img/galeri/pajero.jpeg" class="sm:!col-span-6" alt="galeri 6">
+    </div>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,4 +3,5 @@
     <x-layouts.home.baner />
     <x-layouts.home.advantages />
     <div class="h-[1000px]"></div>
+    <x-layouts.gallery />
 @endsection
