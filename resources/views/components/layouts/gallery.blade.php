9cc928904112ec82ea6d5dbfe1a50cfeaea01504
dion-zebua <zebuadbless@gmail.com>
2025-01-07 22:05:00 +0700

    malas

--- resources/views/components/layouts/gallery.blade.php
+++ resources/views/components/layouts/gallery.blade.php
@@ -8,11 +8,11 @@
     </div>
     <div
         class="grid grid-cols-12 sm:gap-x-6 gap-y-8 [&amp;>*]:overflow-hidden [&amp;>img]:cursor-pointer [&amp;>img]:w-full [&amp;>img]:object-cover [&amp;>img]:rounded-xl [&amp;>img]:shadow-lg [&amp;>img]:h-72 [&amp;>img]:transition-all [&amp;>img]:duration-700 [&amp;>img]:brightness-50 hover:[&amp;>img]:brightness-90 [&amp;>img]:col-span-full">
-        <img width="100" height="100" src="./src/img/galeri/hiace.jpg" class="sm:!col-span-8" alt="galeri 1">
-        <img width="100" height="100" src="./src/img/galeri/surabaya.jpg" class="sm:!col-span-4" alt="galeri 2">
-        <img width="100" height="100" src="./src/img/galeri/bali.jpg" class="sm:!col-span-4" alt="galeri 3">
-        <img width="100" height="100" src="./src/img/galeri/avanza.jpg" class="sm:!col-span-8" alt="galeri 4">
-        <img width="100" height="100" src="./src/img/galeri/grandmax.jpg" class="sm:!col-span-6" alt="galeri 5">
-        <img width="100" height="100" src="./src/img/galeri/pajero.jpeg" class="sm:!col-span-6" alt="galeri 6">
+        <img width="100" height="100" src="{{ asset('img/gallery/image1.png') }}" class="sm:!col-span-8" alt="galeri 1">
+        <img width="100" height="100" src="{{ asset('img/gallery/image2.png') }}" class="sm:!col-span-4" alt="galeri 2">
+        <img width="100" height="100" src="{{ asset('img/gallery/image3.png') }}" class="sm:!col-span-4" alt="galeri 3">
+        <img width="100" height="100" src="{{ asset('img/gallery/image4.png') }}" class="sm:!col-span-8" alt="galeri 4">
+        <img width="100" height="100" src="{{ asset('img/gallery/image5.png') }}" class="sm:!col-span-6" alt="galeri 5">
+        <img width="100" height="100" src="{{ asset('img/gallery/image6.png') }}" class="sm:!col-span-6" alt="galeri 6">
     </div>
 </div>
