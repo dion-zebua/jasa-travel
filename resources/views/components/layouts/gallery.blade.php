5d8a3a0d7a9df3451ef24cb3c27436ca08c803a3
dion-zebua <zebuadbless@gmail.com>
2025-01-07 22:11:23 +0700

    malas

--- resources/views/components/layouts/gallery.blade.php
+++ resources/views/components/layouts/gallery.blade.php
@@ -1,18 +1,24 @@
-<div class="__container">
-    <div class="text-center inner-container">
-        <h2 class="line-h2">Galeri Travel</h2>
-        <p class="desc">
+<div class="py-24 __container">
+    <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="text-base font-semibold leading-7 text-blue-600">Galeri Travel</h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Abadikan Momen Perjalanan Anda</p>
+        <p class="mt-6 text-gray-600">
             Galeri ini menampilkan berbagai unit, foto, dan momen berharga
-            yang merefleksikan perjalanan kami.
-        </p>
+            yang merefleksikan perjalanan kami.</p>
     </div>
     <div
-        class="grid grid-cols-12 sm:gap-x-6 gap-y-8 [&amp;>*]:overflow-hidden [&amp;>img]:cursor-pointer [&amp;>img]:w-full [&amp;>img]:object-cover [&amp;>img]:rounded-xl [&amp;>img]:shadow-lg [&amp;>img]:h-72 [&amp;>img]:transition-all [&amp;>img]:duration-700 [&amp;>img]:brightness-50 hover:[&amp;>img]:brightness-90 [&amp;>img]:col-span-full">
-        <img width="100" height="100" src="{{ asset('img/gallery/image1.png') }}" class="sm:!col-span-8" alt="galeri 1">
-        <img width="100" height="100" src="{{ asset('img/gallery/image2.png') }}" class="sm:!col-span-4" alt="galeri 2">
-        <img width="100" height="100" src="{{ asset('img/gallery/image3.png') }}" class="sm:!col-span-4" alt="galeri 3">
-        <img width="100" height="100" src="{{ asset('img/gallery/image4.png') }}" class="sm:!col-span-8" alt="galeri 4">
-        <img width="100" height="100" src="{{ asset('img/gallery/image5.png') }}" class="sm:!col-span-6" alt="galeri 5">
-        <img width="100" height="100" src="{{ asset('img/gallery/image6.png') }}" class="sm:!col-span-6" alt="galeri 6">
+        class="grid grid-cols-12 sm:gap-x-6 gap-y-8 mt-10 [&>*]:overflow-hidden [&>img]:cursor-pointer [&>img]:w-full [&>img]:object-cover [&>img]:rounded-xl [&>img]:shadow-lg [&>img]:h-72 [&>img]:transition-all [&>img]:duration-700 [&>img]:brightness-50 hover:[&>img]:brightness-90 [&>img]:col-span-full">
+        <img width="100" height="100" src="{{ asset('img/gallery/image1.png') }}" class="sm:!col-span-8"
+            alt="galeri 1">
+        <img width="100" height="100" src="{{ asset('img/gallery/image2.png') }}" class="sm:!col-span-4"
+            alt="galeri 2">
+        <img width="100" height="100" src="{{ asset('img/gallery/image3.png') }}" class="sm:!col-span-4"
+            alt="galeri 3">
+        <img width="100" height="100" src="{{ asset('img/gallery/image4.png') }}" class="sm:!col-span-8"
+            alt="galeri 4">
+        <img width="100" height="100" src="{{ asset('img/gallery/image5.png') }}" class="sm:!col-span-6"
+            alt="galeri 5">
+        <img width="100" height="100" src="{{ asset('img/gallery/image6.png') }}" class="sm:!col-span-6"
+            alt="galeri 6">
     </div>
 </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,5 @@
 @section('content')
     <x-layouts.home.baner />
     <x-layouts.home.advantages />
-    <div class="h-[1000px]"></div>
     <x-layouts.gallery />
 @endsection
