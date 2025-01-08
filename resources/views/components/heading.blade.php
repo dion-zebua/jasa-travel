9bcbb1f41067679b2095f052bbebb97080fc7c0e
dion-zebua <zebuadbless@gmail.com>
2025-01-08 10:21:28 +0700

    malas

---
+++ resources/views/components/heading.blade.php
@@ -0,0 +1,5 @@
+<div class="mx-auto max-w-2xl lg:text-center">
+    <h2 class="text-base font-semibold leading-7 text-blue-600">{{ $title }}</h2>
+    <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ $desc1 }}</p>
+    <p class="mt-6 text-gray-600">{{ $desc2 }}</p>
+</div>
