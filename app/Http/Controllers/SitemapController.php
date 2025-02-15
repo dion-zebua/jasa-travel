19f5f332e109fb638fecdd0f21033ddbe690b197
dion-zebua <zebuadbless@gmail.com>
2025-02-15 10:59:14 +0700

    malas

---
+++ app/Http/Controllers/SitemapController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class SitemapController extends Controller
+{
+    public function static_page()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\SitemapController;
 use App\Http\Controllers\ThumbnailController;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\App;
@@ -42,4 +43,9 @@ Route::controller(LandingPageController::class)->group(function () {
         Route::get('/', 'agenTravel')->name('agen-travel');
         Route::get('/thumbnail.jpg', 'agenTravel')->name('thumbnail-agen-travel');
     });
+});
+
+
+Route::controller(SitemapController::class)->group(function () {
+    Route::get('/sitemap.xml', 'static_page');
 });
